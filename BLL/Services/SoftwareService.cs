9ab189342ff04ab82f8df4f9b522254ebdd79ab4
Whyme <abd_rus_kr@mail.ru>
2025-12-18 13:46:56 +0200

    NewUPdate3.0

---
+++ BLL/Services/SoftwareService.cs
@@ -0,0 +1,37 @@
+﻿using EquipmentAccounting.DAL.Entities;
+using EquipmentAccounting.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class SoftwareService
+    {
+        private readonly IRepository<InstalledSoftware> _softwareRepo;
+        private readonly IRepository<License> _licenseRepo;
+
+        public SoftwareService(IRepository<InstalledSoftware> softwareRepo, IRepository<License> licenseRepo)
+        {
+            _softwareRepo = softwareRepo;
+            _licenseRepo = licenseRepo;
+        }
+
+        public InstalledSoftware InstallSoftware(string name, int employeeId)
+        {
+            var software = new InstalledSoftware
+            {
+                Name = name,
+                EmployeeId = employeeId,
+                InstallDate = DateTime.Now
+            };
+
+            _softwareRepo.Add(software);
+            _softwareRepo.Save();
+
+            return software;
+        }
+    }
+}
